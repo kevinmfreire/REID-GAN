14dc1f6bb66c3d6ddc7c666b98830cfb18927e7f
kevinfreire <kfreirea95@gmail.com>
2021-11-17 23:06:32 -0500

    added TPU compatability mode

--- train.py
+++ train.py
@@ -20,6 +20,12 @@ from loss import *
 from networks import *
 from loader import get_loader
 
+# Importing TPU modules
+import torch_xla.core.xla_model as xm
+import torch_xla.distributed.parallel_loader as pl
+import torch_xla.distributed.xla_multiprocessing as xmp
+import torch_xla.utils.serialization as xser
+
 
 parser= argparse.ArgumentParser()
 
@@ -36,9 +42,9 @@ parser.add_argument('--decay_iters', type=int, default=1000)
 
 parser.add_argument('--transform', type=bool, default=False)
 # if patch training, batch size is (--patch_n * --batch_size)
-parser.add_argument('--patch_n', type=int, default=4)		# default = 4
+parser.add_argument('--patch_n', type=int, default=8)		# default = 4
 parser.add_argument('--patch_size', type=int, default=128)	# default = 100
-parser.add_argument('--batch_size', type=int, default=5)	# default = 5
+parser.add_argument('--batch_size', type=int, default=10)	# default = 5
 parser.add_argument('--image_size', type=int, default=512)
 
 parser.add_argument('--lr', type=float, default=1e-3) # Defailt = 1e-3
@@ -46,6 +52,8 @@ parser.add_argument('--lr', type=float, default=1e-3) # Defailt = 1e-3
 parser.add_argument('--num_epochs', type=int, default=100)
 parser.add_argument('--num_workers', type=int, default=7)
 parser.add_argument('--load_chkpt', type=bool, default=False)
+
+parser.add_argument('--tpu_mode', type=bool, default=True)
 
 args = parser.parse_args()
 
@@ -66,17 +74,35 @@ Tensor = torch.cuda.FloatTensor if cuda_is_present else torch.FloatTensor
 def to_cuda(data):
     	return data.cuda() if cuda_is_present else data
 
+# For training on TPU if available
+tpu_mode = args.tpu_mode
+
+if tpu_mode == True:
+	device = xm.xla_device()
+	data_loader = pl.MpDeviceLoader(data_loader, device)
+
+# For TPU optimizers
+def opt_step(optimizer):
+    	return xm.optimizer_step(optimizer) if tpu_mode == True else optimizer.step()
+
+def save_model(model, dir):
+    	return torch.save(model, dir) if tpu_mode == False else xser.save(model, dir)
+
+def load_model(path):
+    	return torch.load(path) if tpu_mode == False else xser.load(path)
+
+
 image_size = args.image_size if args.patch_size == False else args.patch_size
 
 if args.load_chkpt:
 	print('Loading Chekpoint')
-	whole_model = torch.load(args.save_path+ 'latest_ckpt.pth.tar')
+	whole_model = load_model(args.save_path+ 'latest_ckpt.pth.tar')
 	netG_state_dict,optG_state_dict = whole_model['netG_state_dict'], whole_model['optG_state_dict']
 	netD_state_dict,optD_state_dict = whole_model['netD_state_dict'], whole_model['optD_state_dict']
 	g_net = GNet(image_size)
-	g_net = to_cuda(g_net)
+	g_net = to_cuda(g_net) if tpu_mode == False else g_net.to(device)
 	d_net = DNet()
-	d_net = to_cuda(d_net)
+	d_net = to_cuda(d_net) if tpu_mode == False else d_net.to(device)
 	optimizer_generator = torch.optim.Adam(g_net.parameters())
 	optimizer_discriminator= torch.optim.Adam(d_net.parameters())
 	d_net.load_state_dict(netD_state_dict)
@@ -86,15 +112,15 @@ if args.load_chkpt:
 	cur_epoch = whole_model['epoch']
 	total_iters = whole_model['total_iters']
 	lr = whole_model['lr']
-	g_net = torch.nn.DataParallel(g_net, device_ids=[0, 1])
-	d_net = torch.nn.DataParallel(d_net, device_ids=[0, 1])
+	# g_net = torch.nn.DataParallel(g_net, device_ids=[0, 1])
+	# d_net = torch.nn.DataParallel(d_net, device_ids=[0, 1])
 	print('Current Epoch:{}, Total Iters: {}, Learning rate: {}, Batch size: {}'.format(cur_epoch, total_iters, lr, args.batch_size))
 else:
 	print('Training model from scrath')
 	g_net = GNet(image_size)
-	g_net = to_cuda(g_net)
+	g_net = to_cuda(g_net) if tpu_mode == False else g_net.to(device)
 	d_net = DNet()
-	d_net = to_cuda(d_net)
+	d_net = to_cuda(d_net) if tpu_mode == False else d_net.to(device)
 	optimizer_generator = torch.optim.Adam(g_net.parameters(), lr=args.lr)
 	optimizer_discriminator = torch.optim.Adam(d_net.parameters(), lr=4*args.lr)
 	cur_epoch = 0
@@ -103,9 +129,9 @@ else:
 
 # Losses
 Dloss = DLoss()
-Dloss = to_cuda(Dloss)
+Dloss = to_cuda(Dloss) if tpu_mode == False else Dloss.to(device)
 Gloss = GLoss()
-Gloss = to_cuda(Gloss)
+Gloss = to_cuda(Gloss) if tpu_mode == False else Dloss.to(device)
 
 losses = []
 torch.autograd.set_detect_anomaly(True)
@@ -120,18 +146,19 @@ for epoch in range(cur_epoch, args.num_epochs):
 		x = x.unsqueeze(0).float()
 		y = y.unsqueeze(0).float()
 
-		# patch training
+		# If patch training
 		if args.patch_size:
 			x = x.view(-1, 1, args.patch_size, args.patch_size)
 			y = y.view(-1, 1, args.patch_size, args.patch_size)
 
-		# batch training without any patch size
+		# If batch training without any patch size
 		if args.batch_size and args.patch_size == False:
 			x = x.view(-1, 1, shape_, shape_)
 			y = y.view(-1, 1, shape_, shape_)
 
-		x = to_cuda(x)
-		y = to_cuda(y)
+		if tpu_mode == False:
+			x = to_cuda(x)
+			y = to_cuda(y)
 
 		# NEW PREDICTIONS
 		pred = g_net(x)
@@ -143,14 +170,16 @@ for epoch in range(cur_epoch, args.num_epochs):
 		Dg = d_net(pred)
 		dloss = Dloss(Dy,Dg)
 		dloss.backward(retain_graph=True)
-		optimizer_discriminator.step()
+		# optimizer_discriminator.step()
+		opt_step(optimizer_discriminator)
 
 		# TRAINING GENERATOR
 		optimizer_generator.zero_grad()
 		g_net.zero_grad()
 		gloss = Gloss(Dg, pred, y)
 		gloss.backward()
-		optimizer_generator.step()
+		# optimizer_generator.step()
+		opt_step(optimizer_generator)
 
 		# Print
 		if total_iters % args.print_iters == 0:
@@ -181,7 +210,14 @@ for epoch in range(cur_epoch, args.num_epochs):
 				'lr': lr,
 				'total_iters': total_iters
 			}
-			torch.save(saved_model, '{}iter_{}_ckpt.pth.tar'.format(args.save_path, total_iters))
-			torch.save(saved_model, '{}latest_ckpt.pth.tar'.format(args.save_path))
-	torch.save(saved_model, '{}epoch_{}_ckpt.pth.tar'.format(args.save_path, epoch))
+			# torch.save(saved_model, '{}iter_{}_ckpt.pth.tar'.format(args.save_path, total_iters))
+			# torch.save(saved_model, '{}latest_ckpt.pth.tar'.format(args.save_path))
+			save_model(saved_model, '{}latest_ckpt.pth.tar'.format(args.save_path))
+			cmd = 'cp {}latest_ckpt.pth.tar /gdrive/MyDrive/model/'.format(args.save_path)
+			os.system(cmd)
+	# Saving model after every epoch
+	cmd1 = 'cp {}latest_ckpt.pth.tar /gdrive/MyDrive/model/epoch_{}_ckpt.pth.tar'.format(args.save_path, epoch)
+	cmd2 = 'cp {}latest_ckpt.pth.tar /gdrive/MyDrive/model/'.format(args.save_path)
+	os.system(cmd1)
+	os.system(cmd2)
 	losses.append((gloss.item(), dloss.item()))
