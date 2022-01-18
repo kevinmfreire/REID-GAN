ea39f7ba64223ca551b7cc1ada4c5ccf3fd6e1a9
kevinfreire <kfreirea95@gmail.com>
2022-01-17 19:11:24 -0500

    training with only MSE loss

--- result.py
+++ result.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 path = './deconv_model/'
-total_loss = np.load(path + 'total_loss.npy', allow_pickle=True)
+# total_loss = np.load(path + 'total_loss.npy', allow_pickle=True)
+total_loss = np.load(path + 'loss_arr.npy', allow_pickle=True)
 # loss = np.load(path + 'loss_arr (2).npy', allow_pickle=True)
 # total_loss1 = np.concatenate((total_loss, loss[:5]), axis=0)
 # save_loss = path + 'total_loss.npy'
