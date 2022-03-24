ed4071ff638f4a7611d3d09d4eb22767e77fb794
kevinfreire <kfreirea95@gmail.com>
2022-03-24 19:20:08 -0400

    went back to only 1 block per inception block

--- train.py
+++ train.py
@@ -42,7 +42,7 @@ parser.add_argument('--transform', type=bool, default=False)
 # if patch training, batch size is (--patch_n * --batch_size)
 parser.add_argument('--patch_n', type=int, default=10)		# default = 4
 parser.add_argument('--patch_size', type=int, default=128)	# default = 100
-parser.add_argument('--batch_size', type=int, default=5)	# default = 5
+parser.add_argument('--batch_size', type=int, default=2)	# default = 5
 parser.add_argument('--image_size', type=int, default=512)
 
 parser.add_argument('--lr', type=float, default=2e-4) # 5e-5 without decaying rate
